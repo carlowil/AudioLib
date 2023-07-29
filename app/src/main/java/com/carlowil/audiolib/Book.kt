cda1f21cca50150c36a3185752ebf02093af4517
carlowil <matveysizov84@gmail.com>
2023-07-30 00:01:55 +0300

    I cant manage to get threw all files in Firebase

--- app/src/main/java/com/carlowil/audiolib/Book.kt
+++ app/src/main/java/com/carlowil/audiolib/Book.kt
@@ -3,6 +3,7 @@ package com.carlowil.audiolib
 data class Book(
     val name : String,
     val author : String,
+    val genre : String,
     val description : String,
     val img : String,
     val File : String
